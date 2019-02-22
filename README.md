ba5b755399bb3dc6389b3b56049bb6b25adb227f
Ivan Paulovich <ivan@paulovich.net>
2019-02-22 23:26:56 +0100

    Review

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # What is MrRobot
+It is a Cleaning Robot that informs the number of places cleaned after each execution.
 
-# Running Tests
+# :rocket: Running Unit Tests
 
 ```
 dotnet test tests/MrRobot.UnitTests/MrRobot.UnitTests.csproj
+```
+# :airplane: Running Tests in Terminal
+
+```
+chmod 777 runtests.sh
+./runtests.sh
+```
+
+Expected Output:
+
+```
+=> Cleaned: 4
+=> Cleaned: 40
+=> Cleaned: 11
+=> Cleaned: 800000
 ```
---
+++ source/MrRobot.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MrRobot.Domain.Cleaning.Run;
+
+namespace MrRobot.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCommands = Convert.ToInt32(Console.ReadLine());
+            string positionText = Console.ReadLine();
+            Position initialPosition = new Position(
+                Convert.ToInt32(positionText.Split(' ')[0]), 
+                Convert.ToInt32(positionText.Split(' ')[1]));
+
+            Command[] commands = new Command[numCommands];
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string commandText = Console.ReadLine();
+
+                Direction direction = Direction.North;
+                
+                if (commandText.Split(' ')[0] == "N")
+                    direction = Direction.North;
+                
+                if (commandText.Split(' ')[0] == "S")
+                    direction = Direction.South;
+                
+                if (commandText.Split(' ')[0] == "E")
+                    direction = Direction.East;
+
+                if (commandText.Split(' ')[0] == "W")
+                    direction = Direction.West;
+                
+                commands[i] = new Command(
+                    Convert.ToInt32(commandText.Split(' ')[1]),
+                    direction);
+            }
+
+            IRunUseCase run = new RunUseCase();
+            RunOutput output = run.Execute(initialPosition, commands);
+
+            Console.WriteLine($"=> Cleaned: {output.UniquePlacesCleaned}");
+        }
+    }
+}
